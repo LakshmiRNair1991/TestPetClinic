f5e7662e7ce53fca839bddad3783689d563512ef
LakshmiRNair1991 <lakshmirnair1991@gmail.com>
2025-11-29 21:22:56 +0100

    check screenshot

--- testPetClinic/src/main/java/spring/testPetClinic/actionDriver/Action.java
+++ testPetClinic/src/main/java/spring/testPetClinic/actionDriver/Action.java
@@ -192,7 +192,7 @@ public class Action extends BaseClass {
 		// This new path for jenkins
 		String newImageString = "http://localhost:8080/job/Magneto_CICD/ws/Magneto-Store/ScreenShots/" + filename + "_"
 				+ dateName + ".png";
-		return newImageString;
+		return destination;
 	}
 
 }
